3de39a288a32fbd455f2eb733e52858eee6423d1
Azhar Shah <azharshahstl@yahoo.com>
2019-04-24 16:18:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,28 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get "/" do 
+    erb :index
+  end
+  
+  get "/new" do 
+    erb :create_puppy 
+  end
+    
+  
+  post "/puppy" do
+    puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    @name = puppy.name 
+    @breed = puppy.breed 
+    @age = puppy.months_old
+    binding.pry
+    erb :display_puppy
+  end
+    
+  
+  
+  
+  
 
 end
